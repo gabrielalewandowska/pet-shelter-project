df8693bacdeec4e3e5477802cd50505f25a4cd11
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-08-27 12:31:02 +0100

    Add create

--- controller.rb
+++ controller.rb
@@ -8,3 +8,23 @@ also_reload("./models/*")
 get '/' do
   erb(:home)
 end
+
+get '/animals' do
+  @animals = Pet.all
+  erb(:animals)
+end
+
+get '/animals/new' do
+  @animals = Pet.all
+  erb(:new)
+end
+
+post '/animals' do
+  @pet = Pet.new(params)
+  @pet.save
+  erb(:create)
+end
+
+get '/animals/:id' do
+  @pet = 
+end
